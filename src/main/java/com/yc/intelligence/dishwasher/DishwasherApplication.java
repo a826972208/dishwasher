585ccfb75fd64d87b0dc7422c29dd1148a09f369
yuchao <826972208@qq.com>
2019-07-29 15:22:43 +0800

    Initial commit

---
+++ src/main/java/com/yc/intelligence/dishwasher/DishwasherApplication.java
@@ -0,0 +1,13 @@
+package com.yc.intelligence.dishwasher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishwasherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DishwasherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yc/intelligence/dishwasher/DishwasherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yc.intelligence.dishwasher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DishwasherApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
