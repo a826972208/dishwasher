d63a89e77406899318a515e6949db20dfb896c34
yuchao <826972208@qq.com>
2019-09-23 17:24:43 +0800

    .

--- src/main/java/com/yc/intelligence/dishwasher/DishwasherApplication.java
+++ src/main/java/com/yc/intelligence/dishwasher/DishwasherApplication.java
@@ -3,10 +3,13 @@ package com.yc.intelligence.dishwasher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DishwasherApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
         SpringApplication.run(DishwasherApplication.class, args);
     }
 
